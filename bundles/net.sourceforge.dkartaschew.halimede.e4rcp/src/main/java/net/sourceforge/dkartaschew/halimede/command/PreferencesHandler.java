bc06165b8cffe8c071d7d1f0ed9493f069edc306
Darran Kartaschew <dkartaschew@users.sourceforge.net>
2018-07-20 19:25:11 +1000

    #16 Implement handler for macOS Preference Menu Item
    Add a basic handler.

---
+++ bundles/net.sourceforge.dkartaschew.halimede.e4rcp/src/main/java/net/sourceforge/dkartaschew/halimede/command/PreferencesHandler.java
@@ -0,0 +1,34 @@
+/*-
+ * Halimede Certificate Manager Plugin for Eclipse 
+ * Copyright (C) 2017-2018 Darran Kartaschew 
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v20.html
+ * 
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License, v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is
+ * available at https://www.gnu.org/software/classpath/license.html.
+ */
+
+package net.sourceforge.dkartaschew.halimede.command;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Shell;
+
+public class PreferencesHandler {
+
+	@Execute
+	public void execute(Shell shell) {
+		MessageDialog.openInformation(shell, "Preferences", //
+				"Preferences are set per Certificate Authority. " + System.lineSeparator()
+						+ "Please right-click on the Certificate Authority to "
+						+ "set the preferences for that Certificate Authority." + System.lineSeparator()
+						+ "Halimede currently does not implement a global perference system.");
+	}
+}
