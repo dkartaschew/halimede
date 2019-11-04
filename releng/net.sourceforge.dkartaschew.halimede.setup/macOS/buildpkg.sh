89001a59294096bdbf90055bee6ec29677a244b5
Darran Kartaschew <dkartaschew@users.sourceforge.net>
2019-11-04 20:28:07 +1000

    Start macOS PKG installer

---
+++ releng/net.sourceforge.dkartaschew.halimede.setup/macOS/buildpkg.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+_JRE="OpenJDK11U-jre_x64_mac_hotspot_11.0.5_10.tar.gz"
+_URL="https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.5%2B10/OpenJDK11U-jre_x64_mac_hotspot_11.0.5_10.tar.gz"
+_JRE_FOLDER="jdk-11.0.5+10-jre"
+_BUILD=`date '+%Y%m%d'`
+
+# Clean up
+rm -fR "Halimede CA.app"
+
+# Get JRE
+if [ ! -f "${_JRE}" ]; then
+    wget ${_URL}
+fi
+
+# Extract the zip
+unzip "Halimede CA-macosx.cocoa.x86_64.zip"
+tar -xzf ${_JRE}
+mv ${_JRE_FOLDER} jre
+mv jre "Halimede CA.app/"
+
+# Code sign application
+# TODO:
+
+/usr/local/bin/packagesbuild -v --package-version "1.0.0.$_BUILD" "Halimede CA.pkgproj"
+
+# Code sign installation package
+# TODO:
+
+mv "build/Halimede CA.pkg" ./
+
+# Clean up
+rm -fR "Halimede CA.app"
+rm -fR build
