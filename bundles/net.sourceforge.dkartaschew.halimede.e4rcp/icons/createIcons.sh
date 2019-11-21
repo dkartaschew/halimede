12b6e309b5dd359c20dc3c9541b45d71a96fc198
Darran Kartaschew <dkartaschew@users.sourceforge.net>
2019-11-21 17:51:45 +1000

    Add icon for self signed button on toolbar

--- bundles/net.sourceforge.dkartaschew.halimede.e4rcp/icons/createIcons.sh
+++ bundles/net.sourceforge.dkartaschew.halimede.e4rcp/icons/createIcons.sh
@@ -5,3 +5,8 @@ inkscape -z -e lockall.png -w 16 -h 16 deadlock_ovr.svg;
 inkscape -z -e lockall@1.5x.png -w 24 -h 24 deadlock_ovr.svg; 
 inkscape -z -e lockall@2x.png -w 32 -h 32 deadlock_ovr.svg; 
 inkscape -z -e lockall@3x.png -w 48 -h 48 deadlock_ovr.svg; 
+
+inkscape -z -e certificate_self.png -w 16 -h 16 certificate_self.svg; 
+inkscape -z -e certificate_self@1.5x.png -w 24 -h 24 certificate_self.svg; 
+inkscape -z -e certificate_self@2x.png -w 32 -h 32 certificate_self.svg; 
+inkscape -z -e certificate_self@3x.png -w 48 -h 48 certificate_self.svg; 
