78571d68a2c61bbc6b1a6b104c10a5e1214f16f4
Darran Kartaschew <dkartaschew@users.sourceforge.net>
2019-11-23 09:19:42 +1000

    Update toolbar icons
    Add CSR icon, update PKCS12 icon

--- bundles/net.sourceforge.dkartaschew.halimede.e4rcp/icons/createIcons.sh
+++ bundles/net.sourceforge.dkartaschew.halimede.e4rcp/icons/createIcons.sh
@@ -21,3 +21,8 @@ inkscape -z -e pkcs12_view@1.5x.png -w 24 -h 24 pkcs12_view.svg;
 inkscape -z -e pkcs12_view@2x.png -w 32 -h 32 pkcs12_view.svg; 
 inkscape -z -e pkcs12_view@3x.png -w 48 -h 48 pkcs12_view.svg; 
 
+inkscape -z -e certificate_req.png -w 16 -h 16 certificate_req.svg; 
+inkscape -z -e certificate_req@1.5x.png -w 24 -h 24 certificate_req.svg; 
+inkscape -z -e certificate_req@2x.png -w 32 -h 32 certificate_req.svg; 
+inkscape -z -e certificate_req@3x.png -w 48 -h 48 certificate_req.svg; 
+
