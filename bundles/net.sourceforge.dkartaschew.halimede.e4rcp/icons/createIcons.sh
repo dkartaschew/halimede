fca1769dc3965128e233809638e3a572d964ac5e
Darran Kartaschew <dkartaschew@users.sourceforge.net>
2019-11-22 21:01:36 +1000

    Update toolbar icons.
    Add View Certificate and View PKCS12 icons.

--- bundles/net.sourceforge.dkartaschew.halimede.e4rcp/icons/createIcons.sh
+++ bundles/net.sourceforge.dkartaschew.halimede.e4rcp/icons/createIcons.sh
@@ -10,3 +10,14 @@ inkscape -z -e certificate_self.png -w 16 -h 16 certificate_self.svg;
 inkscape -z -e certificate_self@1.5x.png -w 24 -h 24 certificate_self.svg; 
 inkscape -z -e certificate_self@2x.png -w 32 -h 32 certificate_self.svg; 
 inkscape -z -e certificate_self@3x.png -w 48 -h 48 certificate_self.svg; 
+
+inkscape -z -e certificate_view.png -w 16 -h 16 certificate_view.svg; 
+inkscape -z -e certificate_view@1.5x.png -w 24 -h 24 certificate_view.svg; 
+inkscape -z -e certificate_view@2x.png -w 32 -h 32 certificate_view.svg; 
+inkscape -z -e certificate_view@3x.png -w 48 -h 48 certificate_view.svg; 
+
+inkscape -z -e pkcs12_view.png -w 16 -h 16 pkcs12_view.svg; 
+inkscape -z -e pkcs12_view@1.5x.png -w 24 -h 24 pkcs12_view.svg; 
+inkscape -z -e pkcs12_view@2x.png -w 32 -h 32 pkcs12_view.svg; 
+inkscape -z -e pkcs12_view@3x.png -w 48 -h 48 pkcs12_view.svg; 
+
