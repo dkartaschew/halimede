f2b396570cdff2a2285e33fb139fc2484fefe620
Darran Kartaschew <dkartaschew@users.sourceforge.net>
2019-11-25 20:29:11 +1000

    Update composite output render HR drawing strategy

--- bundles/net.sourceforge.dkartaschew.halimede.core/src/main/java/net/sourceforge/dkartaschew/halimede/ui/composite/CompositeOutputRenderer.java
+++ bundles/net.sourceforge.dkartaschew.halimede.core/src/main/java/net/sourceforge/dkartaschew/halimede/ui/composite/CompositeOutputRenderer.java
@@ -70,10 +70,13 @@ public class CompositeOutputRenderer extends Composite implements ICertificateOu
 	/**
 	 * Drawing strategy for the line.
 	 */
-	private class HRDrawingStrategy implements AnnotationPainter.IDrawingStrategy {
+	private static class HRDrawingStrategy implements AnnotationPainter.IDrawingStrategy {
 
 		@Override
 		public void draw(Annotation annotation, GC gc, StyledText textWidget, int offset, int length, Color color) {
+			if (annotation == null || !annotation.getType().equals(HRAnnotation.TYPE)) {
+				return;
+			}
 			if (gc != null) {
 				final Color foreground = gc.getForeground();
 
@@ -102,17 +105,48 @@ public class CompositeOutputRenderer extends Composite implements ICertificateOu
 				if (right.x > left.x) {
 
 					// draw the horizontal rule
-					gc.setForeground(getDisplay().getSystemColor(SWT.COLOR_DARK_GRAY));
+					gc.setForeground(color);
 					gc.drawLine(left.x, vcenter, right.x, vcenter);
 				}
 
 				gc.setForeground(foreground);
 			} else {
+				// Note: The following is based on org.eclipse.ui.texteditor.SourceViewerDecorationSupport.IBeamStrategy
+				/*
+				 * The length for HR's is always 0, which causes no redraw to occur in StyledText#redraw(int, int,
+				 * boolean). We try to normally redraw at length of one, and up to the line start of the next line if
+				 * offset is at the end of line. If at the end of the document, we redraw the entire document as the
+				 * offset is behind any content.
+				 */
+				final int contentLength = textWidget.getCharCount();
+				if (offset >= contentLength) {
+					textWidget.redraw();
+					return;
+				}
+
+				char ch = textWidget.getTextRange(offset, 1).charAt(0);
+				if (ch == '\r' || ch == '\n') {
+					// at the end of a line, redraw up to the next line start
+					int nextLine = textWidget.getLineAtOffset(offset) + 1;
+					if (nextLine >= textWidget.getLineCount()) {
+						/*
+						 * Panic code: should not happen, as offset is not the last offset, and there is a delimiter
+						 * character at offset.
+						 */
+						textWidget.redraw();
+						return;
+					}
+
+					int nextLineOffset = textWidget.getOffsetAtLine(nextLine);
+					length = nextLineOffset - offset;
+				} else {
+					length = 1;
+				}
 				textWidget.redrawRange(offset, length, true);
 			}
 		}
 	}
-	
+
 	/**
 	 * Annotation service.
 	 */
@@ -121,10 +155,12 @@ public class CompositeOutputRenderer extends Composite implements ICertificateOu
 		public Object getType(Annotation annotation) {
 			return annotation.getType();
 		}
+
 		@Override
 		public boolean isMultiLine(Annotation annotation) {
 			return true;
 		}
+
 		@Override
 		public boolean isTemporary(Annotation annotation) {
 			return true;
@@ -266,11 +302,11 @@ public class CompositeOutputRenderer extends Composite implements ICertificateOu
 	}
 
 	@Override
-	public Menu getMenu () {
+	public Menu getMenu() {
 		checkWidget();
 		return textArea.getMenu();
 	}
-	
+
 	@Override
 	public void addHeaderLine(String value) {
 		StyleRange range = new StyleRange();
@@ -310,7 +346,7 @@ public class CompositeOutputRenderer extends Composite implements ICertificateOu
 		textArea.append(EOL);
 		currentLength += (value.length() + EOLLength);
 	}
-	
+
 	@Override
 	public void addContentLine(String key, String value) {
 		addContentLine(key, value, false);
