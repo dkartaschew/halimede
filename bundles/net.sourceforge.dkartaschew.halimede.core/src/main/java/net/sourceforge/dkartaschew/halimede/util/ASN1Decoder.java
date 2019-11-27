87e923307cbca71440b0348ebff3197de95c102a
Darran Kartaschew <dkartaschew@users.sourceforge.net>
2019-11-27 21:10:17 +1000

    Start on ASN1 Dump utility

---
+++ bundles/net.sourceforge.dkartaschew.halimede.core/src/main/java/net/sourceforge/dkartaschew/halimede/util/ASN1Decoder.java
@@ -0,0 +1,126 @@
+/*-
+ * Halimede Certificate Manager Plugin for Eclipse 
+ * Copyright (C) 2017-2019 Darran Kartaschew 
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v20.html
+ * 
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License, v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is
+ * available at https://www.gnu.org/software/classpath/license.html.
+ */
+
+package net.sourceforge.dkartaschew.halimede.util;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.MalformedInputException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+
+import org.bouncycastle.asn1.ASN1InputStream;
+import org.bouncycastle.asn1.ASN1Primitive;
+import org.bouncycastle.asn1.util.ASN1Dump;
+import org.bouncycastle.openssl.PEMParser;
+
+import net.sourceforge.dkartaschew.halimede.data.render.ICertificateOutputRenderer;
+
+public class ASN1Decoder {
+
+	/**
+	 * The filename to read to decode as ASN1
+	 */
+	private final Path file;
+
+	/**
+	 * Create a ASN1 decoder
+	 * 
+	 * @param filename The file to decode
+	 * @return A decode
+	 * @throws IOException If the file does not exist, or is not readable.
+	 */
+	public static ASN1Decoder create(Path filename) throws IOException {
+		Objects.requireNonNull(filename, "Filename was null");
+		if (!Files.exists(filename)) {
+			throw new FileNotFoundException("File '" + filename + "' not found.");
+		}
+		if (!Files.isRegularFile(filename) || !Files.isReadable(filename)) {
+			throw new IOException("File '" + filename + "' is not readable.");
+		}
+		return new ASN1Decoder(filename);
+	}
+
+	/**
+	 * Create a new decoder based on the given file
+	 * 
+	 * @param file The file to decode
+	 */
+	private ASN1Decoder(Path file) {
+		this.file = file;
+	}
+
+	/**
+	 * Decode the file as ASN1 to string
+	 * 
+	 * @return The file decoded
+	 * @throws IOException If decoding fails.
+	 */
+	public String decode() throws IOException {
+		StringBuilder sb = new StringBuilder();
+		try (PEMParser parser = new PEMParser(Files.newBufferedReader(file, StandardCharsets.UTF_8))) {
+			Object object = parser.readObject();
+			if (object == null) {
+				// May be plain DER without enough info for PEMParser
+				return decodeDER();
+			}
+			while (object != null) {
+				sb.append(ASN1Dump.dumpAsString(object, true));
+				object = parser.readObject();
+			}
+		} catch (MalformedInputException e) {
+			// May be plain DER without enough info for PEMParser
+			return decodeDER();
+		}
+		return sb.toString();
+	}
+
+	/**
+	 * Decode the file as ASN1 to string
+	 * 
+	 * @return The file decoded
+	 * @throws IOException If decoding fails.
+	 */
+	private String decodeDER() throws IOException {
+		byte[] data = Files.readAllBytes(file);
+		StringBuilder sb = new StringBuilder();
+		try (ASN1InputStream input = new ASN1InputStream(data)) {
+			ASN1Primitive p;
+			while ((p = input.readObject()) != null) {
+				sb.append(ASN1Dump.dumpAsString(p, true));
+			}
+		}
+		return sb.toString();
+	}
+
+	/**
+	 * Render the decoded contents to the renderer.
+	 * 
+	 * @param renderer The renderer to write to.
+	 */
+	public void render(ICertificateOutputRenderer renderer) {
+		renderer.addHeaderLine("ASN1 Decode");
+		renderer.addContentLine("Filename", file.toString());
+		try {
+			renderer.addContentLine("", decode(), true);
+		} catch (IOException e) {
+			renderer.addContentLine("Failed to decode: " + ExceptionUtil.getMessage(e));
+		}
+	}
+}
---
+++ tests/net.sourceforge.dkartaschew.halimede.test/src/main/java/net/sourceforge/dkartaschew/halimede/util/TestASN1Decoder.java
@@ -0,0 +1,144 @@
+/*-
+ * Halimede Certificate Manager Plugin for Eclipse 
+ * Copyright (C) 2017-2019 Darran Kartaschew 
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v20.html
+ * 
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License, v. 2.0 are satisfied: GNU General Public License, version 2
+ * with the GNU Classpath Exception which is
+ * available at https://www.gnu.org/software/classpath/license.html.
+ */
+
+package net.sourceforge.dkartaschew.halimede.util;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import net.sourceforge.dkartaschew.halimede.TestUtilities;
+import net.sourceforge.dkartaschew.halimede.data.render.TextOutputRenderer;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TestASN1Decoder {
+
+	@Test
+	public void test_DSA_SHA1_DER() throws IOException {
+		test("dsa4096key_der.csr");
+	}
+
+	@Test
+	public void test_DSA_SHA256_DER() throws IOException {
+		test("dsa4096key_sha256_der.csr");
+	}
+
+	@Test
+	public void test_DSA_SHA1_PEM() throws IOException {
+		test("dsa4096key_pem.csr");
+	}
+
+	@Test
+	public void test_DSA_SHA256_PEM() throws IOException {
+		test("dsa4096key_sha256_pem.csr");
+	}
+
+	@Test
+	public void test_PKCS12() throws IOException {
+		test("rsa4096key.p12");
+	}
+
+	@Test
+	public void test_PKCS12Encrypted() throws IOException {
+		test("rsa4096.p12");
+	}
+
+	@Test
+	public void testX509_DER_RSA() throws IOException {
+		test("rsacert.cer");
+	}
+
+	@Test
+	public void testX509_PEM_RSA() throws IOException {
+		test("rsacert.pem");
+	}
+
+	@Test
+	public void test_RSA_DER() throws IOException {
+		test("rsa4096key.der");
+	}
+
+	@Test
+	public void test_RSA_PEM() throws IOException {
+		test("rsa4096key.pem");
+	}
+
+	@Test
+	public void test_RSA_P8_DER() throws IOException {
+		test("rsa4096key_der.p8");
+	}
+
+	@Test
+	public void test_RSA_P8_PEM() throws IOException {
+		test("rsa4096key_pem.p8");
+	}
+
+	@Test
+	public void test_RSA_P8_AES_DER() throws IOException {
+		test("rsa4096key_aes_der.p8");
+	}
+
+	@Test
+	public void test_RSA_P8_AES_PEM() throws IOException {
+		test("rsa4096key_aes_pem.p8");
+	}
+
+	@Test
+	public void test_RSA_P8_DES_DER() throws IOException {
+		test("rsa4096key_des_der.p8");
+	}
+
+	@Test
+	public void test_RSA_P8_DES_PEM() throws IOException {
+		test("rsa4096key_des_pem.p8");
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void testNullFilename() throws IOException {
+		ASN1Decoder.create(null);
+	}
+	
+	@Test(expected = IOException.class)
+	public void testInvalidFilename() throws IOException {
+		Path file = TestUtilities.getFile("rsa4096.p12").getParent();
+		ASN1Decoder.create(file);
+	}
+	
+	@Test(expected = FileNotFoundException.class)
+	public void testMissingFilename() throws IOException {
+		ASN1Decoder.create(Paths.get("ras4096.p12"));
+	}
+
+	private void test(String filename) throws IOException {
+		Path file = TestUtilities.getFile(filename);
+		ASN1Decoder d = ASN1Decoder.create(file);
+		String decode = d.decode();
+		assertNotNull(decode);
+		assertFalse(decode.isEmpty());
+		assertFalse(decode, decode.contains("unknown object"));
+		System.out.println(decode);
+		d.render(new TextOutputRenderer(System.out));
+	}
+}
